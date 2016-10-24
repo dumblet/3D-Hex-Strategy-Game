8066032a07d10a440ebe9b8cf94743cfa5160691
dumblet <miah.jere18@gmail.com>
2016-10-24 01:07:06 -0700

    just finished Ep 3 of the guide, can switch colors of tiles to red, can detect tiles with the mouse, and has tiles (probably put this list in the opposite order)

---
+++ 3D Hex Based Strategy Game/Assets/Scripts/Map.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+
+	public GameObject hexPrefab;
+
+	//size of the map by hex tiles
+	int width = 20;
+	int height = 20;
+
+	//offsets to line up hex tiles
+	float xOffset = 1.77f;
+	float zOffset = 1.531f;
+
+	// Use this for initialization
+	void Start () {
+	
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+
+				float xPos = x * xOffset;
+
+				if (y % 2 == 1) {
+					xPos += xOffset/2;
+				}
+
+				GameObject hex_go = (GameObject)Instantiate (hexPrefab, new Vector3(xPos,0, y * zOffset), Quaternion.identity);
+
+				hex_go.name = "Hex_" + x + "_" + y;
+
+				hex_go.transform.SetParent (this.transform);
+			
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 3D Hex Based Strategy Game/Assets/Scripts/MouseManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);  //shoots a ray from camera to mouse position
+		RaycastHit hitInfo; //hitInfo instantiation is required so Raycast can output the object it is hitting
+
+		if (Physics.Raycast(ray, out hitInfo)) {
+			GameObject ourHitObject = hitInfo.collider.transform.gameObject;
+
+			Debug.Log ("Raycast hit " + ourHitObject.name); // transform parent name to get the object hexagon's parent which has the more useful name
+
+			if (Input.GetButton(0)) {
+				MeshRenderer mr = ourHitObject.GetComponentsInChildren<MeshRenderer> ();
+				mr.material.color = Color.red
+			}
+		}
+
+		//for an FPS, it would look like this
+		//Ray fpsRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+		//shoots out a ray from the camera to the center of the screen.
+	}
+}
